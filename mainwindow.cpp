b3b2660bb0fbadcdcf407f60aa6a68691c84cfda
ammelto <alexander.melton@okstate.edu>
2015-01-17 23:28:05 -0600

    Added new UI elements

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,8 +12,8 @@ MainWindow::MainWindow(QWidget *parent) :
     scene = new QGraphicsScene(this);
     scene->setItemIndexMethod(QGraphicsScene::NoIndex);
     ui->field->setScene(scene);
-    Ball *ball = new Ball(30,30,Qt::red,20,0,false);
-    scene->addItem(ball);
+    //Ball *ball = new Ball(30,30,Qt::red,20,0,false);
+    //scene->addItem(ball);
 
 
 
